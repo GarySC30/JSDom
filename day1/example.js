92b0ca8ef180d4b9501dd5272f403de8d648c274
GarySC30 <30978491+GarySC30@users.noreply.github.com>
2021-04-03 19:05:41 +0800

    2.2.5 数组

--- day1/example.js
+++ day1/example.js
@@ -9,3 +9,15 @@ var height = "about 5'\" tall"
 
 var bool = true
 var char = "true" // 两者含义不同
+
+var beatles = Array(5)  // 使用关键字Array来定义数组
+var beatles1 = Array()  // 可以使用不限定长度的数组
+
+// 填充数组可以逐个填充或者定义时填充
+var a1 = Array(2)
+a(0) = "java"
+a(1) = "script";
+
+var a2 = Array("java", "scrpt");
+
+var a3 = ["java", "scrpt"];
